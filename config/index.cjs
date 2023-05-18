e291e56a5031ad15cc226eef3b1d1f1f15be3ffa
Lizi2856 <133195039+Lizi2856@users.noreply.github.com>
2023-05-19 00:50:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -31,7 +31,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oAfIM64EUn7x91ffuF9wyFUkFlTk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'DHrvjjEj-jI1Fom7gAu3qB8ygjs3FbjaPBQyrcwxLeI',
+      useTemplateId: 'ZS2INCBCR8mC7-9XwEgxAlV_ZI9wo8cuFksmN4lSvT8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -56,7 +56,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'DHrvjjEj-jI1Fom7gAu3qB8ygjs3FbjaPBQyrcwxLeI',
+  CALLBACK_TEMPLATE_ID: 'ZS2INCBCR8mC7-9XwEgxAlV_ZI9wo8cuFksmN4lSvT8',
 
   CALLBACK_USERS: [
     {
